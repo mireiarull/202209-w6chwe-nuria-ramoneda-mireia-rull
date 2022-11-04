ce7e9ffbe1e6db9764bcdd4b1cd50c58f9ef6362
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-04 22:49:08 +0100

    Add loadRobotsActionCreator and empty default test

---
+++ src/mocks/mockRobotsList.ts
@@ -0,0 +1,24 @@
+import Robots from "../types";
+
+const mockRobotsList: Robots[] = [
+  {
+    name: "wall-e",
+    image: "image.jpg",
+    features: {
+      speed: 1,
+      endurance: 1,
+      creationDate: 1,
+    },
+  },
+  {
+    name: "R2D2",
+    image: "image.jpg",
+    features: {
+      speed: 2,
+      endurance: 2,
+      creationDate: 2,
+    },
+  },
+];
+
+export default mockRobotsList;
